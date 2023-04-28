70530d356131834b5229b7e5d5534d0153528656
pj8912 <jpintofun2b@gmail.com>
2023-04-28 13:30:02 +0530

    fixed key creation and import

--- test/test.php
+++ test/test.php
@@ -11,61 +11,71 @@ If You already have a private key import <br>
 <input type="text" id="private_key" placeholder="PrivateKey(nsec)"><br>
 <button id="import" type="button"> Import Key</button>
 
-
 <script>
-    function accountKeys() {
-        fetch('keys.php')
-            .then(res => res.json())
-            .then(response => {
-                console.log(response)
-                let checkNostrData = localStorage.getItem("userNostrData")
-                if (checkNostrData == null) {
-                    const userNostrData = {
-                        "keys": {
-                            "public_key": response.public_key,
-                            "private_key": response.private_key
+
+    document.onreadystatechange = function() {
+        if (document.readyState == "interactive") {
+
+            document.getElementById('import').addEventListener("click", importKey)
+
+            if (localStorage.getItem("userNostrData") === null) {
+                fetch('keys.php')
+                    .then(res => res.json())
+                    .then(response => {
+                        console.log(response)
+                        const userNostrData = {
+                            "keys": {
+                                "public_key": response.public_key,
+                                "private_key": response.private_key
+                            }
                         }
-                    }
-                    localStorage.setItem("userNostrData", JSON.stringify(userNostrData));
-                }
-            })
-            .catch(err => console.log(err))
-    }
-    let checkNostrData = localStorage.getItem("userNostrData")
-    if (checkNostrData !== null) {
-        console.log(JSON.parse(checkNostrData));
-        console.log('')
-        console.log(JSON.parse(checkNostrData).keys['private_key']);
-        console.log(JSON.parse(checkNostrData).keys['public_key']);
-    } else {
-        accountKeys();
-    }
+                        localStorage.setItem("userNostrData", JSON.stringify(userNostrData));
+                    })
+                    .catch(err => console.log(err))
+            } else {
+                let checkNostrData = localStorage.getItem("userNostrData")
+                console.log(JSON.parse(checkNostrData));
+                console.log('')
+                console.log(JSON.parse(checkNostrData).keys['private_key']);
+                console.log(JSON.parse(checkNostrData).keys['public_key']);
+            }
 
-    function importKey() {
+            function importKey() {
+                localStorage.clear()
+                const private_key = document.getElementById('private_key');
 
-        const private_key = document.getElementById('private_key');
+                fetch('import_key.php', {
+                        method: "POST",
+                        body: JSON.stringify({
+                            "input_private_key": private_key.value
+                        })
+                    })
 
-        fetch('import_key.php', {
-                method: "POST",
-                body: JSON.stringify({
-                    "input_private_key": private_key.value
-                })
-            })
-            .then(res => res.json())
-            .then(response => {
-                console.log(`
-               |----------YOUR ACCOUNT-----------------------------------------------------------|
-               | PUBLIC KEY :  ${response.publickey}     |
-               | PRIVATE KEY: ${response.privatekey}     |
-               |---------------------------------------------------------------------------------|`)
-        })
-            .catch(err => console.log('err:' + err))
-            private_key.value='';
+                    .then(res => res.json())
 
-    }
+                    .then(response => {
+                        const userNostrData = {
+                            "keys": {
+                                "public_key": response.publickey,
+                                "private_key": response.privatekey
+                            }
+                        }
+                        localStorage.setItem("userNostrData", JSON.stringify(userNostrData));
+                        let checkNostrData = localStorage.getItem("userNostrData")
+                        console.log('new private key: ' + JSON.parse(checkNostrData).keys['private_key']);
+                        console.log('new public key: ' + JSON.parse(checkNostrData).keys['public_key']);
 
-    document.getElementById('import').onclick =() =>{
-        window.location.reload
-        importKey()
+                        //     console.log(`
+                        //    |----------YOUR ACCOUNT-----------------------------------------------------------|
+                        //    | PUBLIC KEY :  ${response.publickey}     |
+                        //    | PRIVATE KEY: ${response.privatekey}     |
+                        //    |---------------------------------------------------------------------------------|`)
+                    })
+
+                    .catch(err => console.log('err:' + err))
+                private_key.value = '';
+
+            }
+        }
     }
 </script>
